d2288f21ef9ff4214d991c400910deda33596c79
Peter-TMK <tundepeter01@gmail.com>
2024-08-15 08:31:50 +0100

    feat: only ADMINS can perform CRUD on USERS ACCOUNT

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { register, login } = require("./src/controllers/auth.controller");
 const authRouter = require("./src/routes/auth.routes");
 const productRouter = require("./src/routes/product.routes");
 const notificationRouter = require("./src/routes/notification.routes");
+const userRouter = require("./src/routes/user.route");
 
 const app = express();
 app.use(express.json());
@@ -13,5 +14,6 @@ connectDB();
 app.use("/api/auth", authRouter);
 app.use("/api/products", productRouter);
 app.use("/api/products/notification", notificationRouter);
+app.use("/api/user", userRouter);
 
 module.exports = app;
---
+++ src/routes/user.route.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const userRouter = express.Router();
+
+// const {
+//   validateTaskPostMiddleWare,
+//   validateTaskUpdateMiddleWare,
+//   validateUserUpdateMiddleware,
+// } = require("../middleware/validator.middleware");
+
+// const validateObjectID = require("../middleware/validateObjectID.middleware");
+const {
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user.controller");
+
+const authenticate = require("../middleware/auth.middleware");
+const roleMiddleware = require("../middleware/role.middleware");
+
+// userRouter.get("/search", searchTaskByTitle);
+
+userRouter.get("/", authenticate, roleMiddleware("admin"), getAllUsers);
+
+userRouter.get(
+  "/:id",
+  authenticate,
+  roleMiddleware("admin"),
+  // validateObjectID,
+  getSingleUser
+);
+
+// userRouter.post("/", validateTaskPostMiddleWare, postTask);
+
+userRouter.put(
+  "/:id",
+  authenticate,
+  roleMiddleware("admin"),
+  // validateObjectID,
+  // validateUserUpdateMiddleware,
+  updateUser
+);
+
+userRouter.delete("/:id", authenticate, roleMiddleware("admin"), deleteUser);
+
+module.exports = userRouter;
